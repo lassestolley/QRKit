a8263e16f64dc2e26e5dafbeb598d7630914f78c
Lasse Stolley <lasse@technikabo.com>
2021-10-17 11:29:52 +0200

    Added generateQRCode

--- Sources/QRKit/QRKit.swift
+++ Sources/QRKit/QRKit.swift
@@ -1,6 +1,28 @@
-public struct QRKit {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import UIKit
+import CoreImage
+import Foundation
+
+// Generate QR Code
+public func generateQRCode(from string: String) -> UIImage? {
+    
+    // Remove umlauts
+    let removeÄ = string.replacingOccurrences(of: "ä", with: "ae")
+    let removeÜ = removeÄ.replacingOccurrences(of: "ü", with: "ue")
+    let removeÖ = removeÜ.replacingOccurrences(of: "ö", with: "oe")
+    let validString = removeÖ
+    
+    let data = validString.data(using: String.Encoding.ascii)
+    
+    if let filter = CIFilter(name: "CIQRCodeGenerator") {
+        
+        filter.setValue(data, forKey: "inputMessage")
+        let transform = CGAffineTransform(scaleX: 20, y: 20)
+        
+        if let output = filter.outputImage?.transformed(by: transform) {
+            return UIImage(ciImage: output)
+        }
     }
+    
+    return nil
 }
