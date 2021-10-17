c461ae39308ba463b4b595573be6dcc43ba31ed6
Lasse Stolley <lasse@technikabo.com>
2021-10-17 13:22:16 +0200

    QR Code different colors added

--- Sources/QRKit/QRKit.swift
+++ Sources/QRKit/QRKit.swift
@@ -4,7 +4,7 @@ import CoreImage
 import Foundation
 
 // Generate QR Code
-public func generateQRCode(from string: String) -> UIImage? {
+public func generateQRCode(from string: String, backroundColor: UIColor, foregroundColor: UIColor) -> UIImage? {
     
     // Remove umlauts
     let removeÄ = string.replacingOccurrences(of: "ä", with: "ae")
@@ -14,13 +14,26 @@ public func generateQRCode(from string: String) -> UIImage? {
     
     let data = validString.data(using: String.Encoding.ascii)
     
+    // QR filter
     if let filter = CIFilter(name: "CIQRCodeGenerator") {
         
         filter.setValue(data, forKey: "inputMessage")
         let transform = CGAffineTransform(scaleX: 20, y: 20)
         
         if let output = filter.outputImage?.transformed(by: transform) {
-            return UIImage(ciImage: output)
+            
+            // Color filter
+            let colorFilter: CIFilter = CIFilter(name: "CIFalseColor")!
+
+            // Set Colors
+            colorFilter.setDefaults()
+            colorFilter.setValue(output, forKey: "inputImage")
+            colorFilter.setValue(foregroundColor, forKey: "inputColor0")
+            colorFilter.setValue(backroundColor, forKey: "inputColor1")
+
+            let outputImage = colorFilter.outputImage!
+            
+            return UIImage(ciImage: outputImage)
         }
     }
     
